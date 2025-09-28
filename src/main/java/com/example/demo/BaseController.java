b34b5f4eaecde8cf3f3244c80400368f18d75bf4
nhanstackdev <nhanstack.dev@gmail.com>
2025-09-28 09:12:26 +0700

    tutorial: connect database mySql

--- src/main/java/com/example/demo/BaseController.java
+++ src/main/java/com/example/demo/BaseController.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +9,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("v1/api")
 public class BaseController {
+    private final JdbcTemplate jdbcTemplate;
+
+    public BaseController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
     @GetMapping("/test")
     @ResponseBody
     public String test(){
-        return "Hello world !!!";
+
+        String sql = "CREATE TABLE ROLE (" +
+                "id INT AUTO_INCREMENT PRIMARY KEY," +
+                "title VARCHAR(255) NOT NULL" +
+                ")";
+
+        jdbcTemplate.execute(sql);
+
+        return "Tạo bảng thành công";
     }
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,19 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
     	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+            Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+            // Set environment variables
+            dotenv.entries().forEach(entry -> {
+                System.setProperty(entry.getKey(), entry.getValue());
+            });
+
+            SpringApplication.run(DemoApplication.class, args);
 	}
 }
